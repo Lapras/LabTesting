7140d11d5c578df0ee40b3105bcb7fdc12ec4923
Lapras <jermurray2@gmail.com>
2024-07-29 21:15:10 -0500

    stuff

--- SQLServer/DBDump.sql
+++ SQLServer/DBDump.sql
@@ -1,35 +1,6 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 12.18 (Ubuntu 12.18-0ubuntu0.20.04.1)
--- Dumped by pg_dump version 12.18 (Ubuntu 12.18-0ubuntu0.20.04.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: notes; Type: TABLE; Schema: public; Owner: postgres
---
-
 CREATE TABLE public.notes (
     note text
 );
-
-
-ALTER TABLE public.notes OWNER TO postgres;
 
 --
 -- Data for Name: notes; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -42,9 +13,3 @@ Lets note something important
 bob has a port 25 SSH account
 The password is B0bBoY25876
 \.
-
-
---
--- PostgreSQL database dump complete
---
-
